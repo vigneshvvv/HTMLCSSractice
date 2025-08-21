9e5008de0f6a6700e812e8c8f6125c91481bec13
vignesh <vignesh33319@gmail.com>
2025-08-21 16:44:00 +0530

    added json processing notes

--- Registration.js
+++ Registration.js
@@ -22,3 +22,57 @@ let obj = document.getElementById("newForm").addEventListener("submit", function
     console.log(objectN);
     console.log(objectN.firstname);
 })
+let x;
+async function sample() {
+    try{
+        let res = await fetch("sample.json");
+         x = await res.json();
+        console.log(x);
+        return x; 
+    }catch(e){
+        console.error(e);
+    }
+    
+}
+
+async function useData() {
+    let data = await sample();
+    console.log(data[0].name);
+    
+}
+useData();
+
+let obj1 = {
+    name:"vs",
+    proff: "Dev"
+}
+
+for (key in obj1){
+    if(key === "n"){
+        console.log("if got printed");
+    }
+    else if (key === "name") {
+        console.log("else if printed");
+    } else {
+        console.log("got none");
+    }
+    console.log(key);
+    console.log(obj1[key]);
+}
+
+let env;
+async function ecom() {
+    let data4 = await fetch("objectSample.json");
+    let nk = await data4.json();
+      console.log("dataReceived");
+    return nk;
+}
+
+// /ecom().then(e => console.log(e));
+
+async function sample2(){
+    let data = await ecom();
+    console.log(data);
+}
+
+sample2();
